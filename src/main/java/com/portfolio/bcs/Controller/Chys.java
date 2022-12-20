34f4783c1c1fd4d4b03535cab0cdc29e60064241
BenjaminConcha <benjaminconcha1415@gmail.com>
2022-12-20 01:56:37 -0300

    Arreglo del Back

--- src/main/java/com/portfolio/bcs/Controller/Chys.java
+++ src/main/java/com/portfolio/bcs/Controller/Chys.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping("/hys")
+@RequestMapping("/skill")
 @CrossOrigin(origins = "https://ftdbcs-516a6.web.app")
 public class Chys {
 
